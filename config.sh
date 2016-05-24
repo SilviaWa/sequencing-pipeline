9a0d3358a690ba609472a4c6ff3045b1474526f2
SilviaWa <sunnysilvia1016@gmail.com>
2016-05-24 13:57:48 -0500

    add config.sh

---
+++ config.sh
@@ -0,0 +1,8 @@
+# We now require a version of STAR above 2.40a
+STARBIN="/opt/programs/bin/STAR"
+SNAPBIN="/opt/programs/bin/snap"
+SAILFISHBIN="/opt/programs/bin/sailfish"
+SAILFISHLIB="/opt/programs/sailfish/lib"
+TMPDIR="/mnt/datab/tmp"
+REFBASE="/mnt/datab/refs"
+THREADS=30
